4700c2b165491bc970563da6dd91d36516d3235b
Catrin Tornqvist <catrin.tq@gmail.com>
2025-06-04 15:19:27 +0200

    add tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+export const content = [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+];
+export const theme = {
+    extend: {},
+};
+export const darkMode = "class";
+export const plugins = [];
+  
