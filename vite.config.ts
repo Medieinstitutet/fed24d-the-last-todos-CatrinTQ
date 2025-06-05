cd86993ab0acd8ea23e2ddf92ed89bd8a368a497
Catrin Tornqvist <catrin.tq@gmail.com>
2025-06-05 12:10:43 +0200

    add base in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/fed24d-the-last-todos-CatrinTQ/',
   plugins: [
     react(),
     tailwindcss(),
